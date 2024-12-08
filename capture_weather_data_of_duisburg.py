b2493f98442152ac19252b23d1f5dbd4cf408e65
dengwanlin <120671451+dengwanlin@users.noreply.github.com>
2024-12-09 00:36:36 +0100

    Update capture_weather_data_of_duisburg.py

--- capture_weather_data_of_duisburg.py
+++ capture_weather_data_of_duisburg.py
@@ -3,14 +3,14 @@ import pandas as pd
 from datetime import datetime
 
 # Define location and time frame
-location = Point(51.43, 6.77)  # Duisburg 的经纬度
-start = datetime(2023, 1, 1)  # 转换为 datetime 对象
-end = datetime(2023, 12, 31)  # 转换为 datetime 对象
+location = Point(51.43, 6.77)  # Duisburg's latitude and gratitude
+start = datetime(2023, 1, 1)  # convert to datetime object
+end = datetime(2023, 12, 31)  # convert to datetime object
 
 # Get data
 data = Daily(location, start, end)
 data = data.fetch()
 
 # Save to CSV
-data.to_csv("Duisburg_Weather_2023.csv", index=True)  # index=True 保留索引
+data.to_csv("Duisburg_Weather_2023.csv", index=True)  # index=True, keep the index
 print("Weather data saved to Duisburg_Weather_2023.csv")
